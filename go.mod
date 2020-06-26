ba69e3bdc84b7f7e552f7e87c3ae42130600788d
Harish Singh Bohara <harish.bohara@olacabs.com>
2020-06-26 12:06:08 +0530

    initial code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/harishb2k/gox-util
+
+go 1.13
+
+require (
+	github.com/gorilla/handlers v1.4.2
+	github.com/mkevac/debugcharts v0.0.0-20191222103121-ae1c48aa8615
+)
---
+++ profiler_debugcharts.go
@@ -0,0 +1,18 @@
+package goxutil
+
+import (
+    "fmt"
+    "github.com/gorilla/handlers"
+    _ "github.com/gorilla/handlers"
+    _ "github.com/mkevac/debugcharts"
+    "log"
+    "net/http"
+    _ "net/http/pprof"
+)
+
+func LaunchProfileDebugChart(port int) {
+    go func() {
+        log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), handlers.CompressHandler(http.DefaultServeMux)))
+    }()
+    log.Printf("ProfileDebugChart - Running at http://localhost:%d/debug/charts/", port)
+}
